dd6c174a4d955eee2038049273ec7e33d69700de
batpinar <batpnar@gmail.com>
2025-05-02 04:36:23 +0300

    fix: Update assembly informational version and generated comments across multiple projects

--- ConfigurationReader.Tests/obj/Debug/net8.0/ConfigurationReader.Tests.AssemblyInfo.cs
+++ ConfigurationReader.Tests/obj/Debug/net8.0/ConfigurationReader.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConfigurationReader.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b22dc40fb94ee407371f015324f8462377774cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConfigurationReader.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConfigurationReader.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
